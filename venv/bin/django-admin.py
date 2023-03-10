aad2efa328a962f899e6eaceee29f4e57c065bf1
alexdlveliz-ciancoders <erick@ciancoders.com>
2023-03-10 15:35:31 -0600

    agregado venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/Documentos/Ciancoders/Starter/react-redux-starter/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
